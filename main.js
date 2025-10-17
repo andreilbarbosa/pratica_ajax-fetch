b9d4c74060ec0159198bd9bfb124f538b2161230
andrei <andreilimabarbosa@outlook.com>
2025-10-17 17:29:20 -0300

    feat(commit initial): Adiciona a configuração de busca usando o fetch.

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
 
 $(document).ready(function () {
 
-    
+
     $('#btn-cep').click(function () {
         const cep = $('#cep').val();
         const endpoint = `https://viacep.com.br/ws/${cep}/json`;
@@ -20,18 +20,34 @@ $(document).ready(function () {
         $(botao).find('i').addClass('d-none');
         $(botao).find('span').removeClass('d-none');
 
-        $.ajax(endpoint).done(function (resposta) {
-            const logradouro = resposta.logradouro;
-            const bairro = resposta.bairro;
-            const cidade = resposta.localidade;
-            const estado = resposta.uf;
-            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado} `;
+        // $.ajax(endpoint).done(function (resposta) {
+        //     const logradouro = resposta.logradouro;
+        //     const bairro = resposta.bairro;
+        //     const cidade = resposta.localidade;
+        //     const estado = resposta.uf;
+        //     const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado} `;
 
-            setTimeout(function(){
-                $('#endereco').val(endereco);
-                $(botao).find('i').removeClass('d-none');
-                $(botao).find('span').addClass('d-none');
-            }, 1400);
+        //     setTimeout(function(){
+        //         $('#endereco').val(endereco);
+        //         $(botao).find('i').removeClass('d-none');
+        //         $(botao).find('span').addClass('d-none');
+        //     }, 1400);
+        // })
+
+        fetch(endpoint).then(function (resposta) {
+            return resposta.json()
         })
+            .then(function (json) {
+                const logradouro = json.logradouro;
+                const bairro = json.bairro;
+                const cidade = json.localidade;
+                const estado = json.uf;
+                const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado} `;
+                setTimeout(function () {
+                    $('#endereco').val(endereco);
+                    $(botao).find('i').removeClass('d-none');
+                    $(botao).find('span').addClass('d-none');
+                }, 1400);
+            })
     })
 })
